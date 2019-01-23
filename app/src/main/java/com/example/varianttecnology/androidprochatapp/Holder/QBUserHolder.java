fbe8c9770cdd0f22604b57a57d0b91fe8e26f13e
Koushik Saha <Koushik.saha666@gmail.com>
2019-01-23 19:20:26 +0600

    Pro Chat App Design And 1st public chat implementation

---
+++ app/src/main/java/com/example/varianttecnology/androidprochatapp/Holder/QBUserHolder.java
@@ -0,0 +1,53 @@
+package com.example.varianttecnology.androidprochatapp.Holder;
+
+import android.util.SparseArray;
+
+import com.quickblox.users.model.QBUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QBUserHolder {
+
+    private static QBUserHolder instance;
+
+    private SparseArray<QBUser> qbUserSparseArray;
+
+    public static synchronized QBUserHolder getInstance(){
+        if (instance == null)
+            instance = new QBUserHolder();
+        return instance;
+    }
+
+    private QBUserHolder()
+    {
+        qbUserSparseArray = new SparseArray<>();
+    }
+
+    public void putUsers(List<QBUser> users)
+    {
+        for (QBUser user:users)
+            putUser(user);
+    }
+
+    private void putUser(QBUser user){
+        qbUserSparseArray.put(user.getId(),user);
+    }
+
+    public QBUser getUserById(int id)
+    {
+        return qbUserSparseArray.get(id);
+    }
+
+    public List<QBUser> getUsersByIds(List<Integer> ids)
+    {
+        List<QBUser> qbUser = new ArrayList<>();
+        for (Integer id:ids)
+        {
+            QBUser user = getUserById(id);
+            if (user != null)
+                qbUser.add(user);
+        }
+        return qbUser;
+    }
+}
